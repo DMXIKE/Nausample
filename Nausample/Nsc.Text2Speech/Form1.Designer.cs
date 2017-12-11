08fa53e5645b6e6d5c01172b254acfb58e0b4e4f
dmxike <dmxike@yeah.net>
2017-12-11 11:57:59 +0800

    Add Text2Speech Project

---
+++ Nausample/Nsc.Text2Speech/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Nsc.Text2Speech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.btnListen = new System.Windows.Forms.Button();
+            this.tbRate = new System.Windows.Forms.TrackBar();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.tbVolume = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tbRate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbVolume)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtText
+            // 
+            this.txtText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtText.Location = new System.Drawing.Point(2, 2);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(453, 242);
+            this.txtText.TabIndex = 0;
+            // 
+            // btnListen
+            // 
+            this.btnListen.Location = new System.Drawing.Point(12, 250);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(75, 31);
+            this.btnListen.TabIndex = 1;
+            this.btnListen.Text = "试听";
+            this.btnListen.UseVisualStyleBackColor = true;
+            this.btnListen.Click += new System.EventHandler(this.btnListen_Click);
+            // 
+            // tbRate
+            // 
+            this.tbRate.Location = new System.Drawing.Point(361, 250);
+            this.tbRate.Minimum = -10;
+            this.tbRate.Name = "tbRate";
+            this.tbRate.Size = new System.Drawing.Size(93, 45);
+            this.tbRate.TabIndex = 2;
+            this.tbRate.Scroll += new System.EventHandler(this.tbRate_Scroll);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(93, 250);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 31);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // tbVolume
+            // 
+            this.tbVolume.Location = new System.Drawing.Point(225, 250);
+            this.tbVolume.Maximum = 100;
+            this.tbVolume.Name = "tbVolume";
+            this.tbVolume.Size = new System.Drawing.Size(93, 45);
+            this.tbVolume.TabIndex = 4;
+            this.tbVolume.TickFrequency = 5;
+            this.tbVolume.Value = 100;
+            this.tbVolume.Scroll += new System.EventHandler(this.tbVolume_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(193, 259);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "音量:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(329, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "语速:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 285);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbVolume);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tbRate);
+            this.Controls.Add(this.btnListen);
+            this.Controls.Add(this.txtText);
+            this.Name = "Form1";
+            this.Text = "文本转语音";
+            ((System.ComponentModel.ISupportInitialize)(this.tbRate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbVolume)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Button btnListen;
+        private System.Windows.Forms.TrackBar tbRate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TrackBar tbVolume;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Nausample/Nsc.Text2Speech/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Nsc.Text2Speech
+{
+    public partial class Form1 : Form
+    {
+        private readonly string s_start = "试听";
+        private readonly string s_stop = "停止";
+        private SpeechSynthesizer speech;
+        public Form1()
+        {
+            InitializeComponent();
+            this.btnListen.Text = s_start;
+
+            speech = new SpeechSynthesizer
+            {
+                Rate = tbRate.Value,
+                Volume = tbVolume.Value
+            };
+            speech.SpeakCompleted += speech_SpeakCompleted;//绑定事件
+            speech.SelectVoice("Microsoft Lili");//设置播音员（中文）
+            //speech.SelectVoice("Microsoft Anna"); //英文
+        }
+
+        private void btnListen_Click(object sender, EventArgs e)
+        {
+            if (this.btnListen.Text == s_start)
+            {
+                new Thread(Speak).Start();
+                this.btnListen.Text = s_stop;
+            }
+            else
+            {
+                speech.SpeakAsyncCancelAll();
+                this.btnListen.Text = s_start;
+            }
+        }
+
+        private void Speak()
+        {
+            speech.SpeakAsync(txtText.Text);//语音阅读方法
+        }
+
+        private void speech_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            this.btnListen.Text = s_start;
+        }
+
+        private void tbVolume_Scroll(object sender, EventArgs e)
+        {
+            speech.Volume = tbVolume.Value;
+        }
+
+        private void tbRate_Scroll(object sender, EventArgs e)
+        {
+            speech.Rate = tbRate.Value;
+        }
+        private void SaveFile(string text)
+        {
+            speech = new SpeechSynthesizer();
+            var dialog = new SaveFileDialog
+            {
+                Filter = "*.wav|*.wav|*.mp3|*.mp3"
+            };
+            dialog.ShowDialog();
+
+            string path = dialog.FileName;
+            if (path.Trim().Length == 0)
+            {
+                return;
+            }
+            speech.SetOutputToWaveFile(path);
+            speech.Volume = tbVolume.Value;
+            speech.Rate = tbRate.Value;
+            speech.Speak(text);
+            speech.SetOutputToNull();
+            MessageBox.Show("生成成功!在" + path + "路径中！", "提示");
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            string text = txtText.Text;
+
+            if (text.Trim().Length == 0)
+            {
+                MessageBox.Show("空内容无法生成!", "错误提示");
+                return;
+            }
+
+            this.SaveFile(text);
+
+        }
+    }
+}
